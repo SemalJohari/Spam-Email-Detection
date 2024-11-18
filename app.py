a4a060c55697dc19cb7432c9e1543ce6d41d67a9
Semal Johari <100789482+SemalJohari@users.noreply.github.com>
2024-11-18 19:30:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,5 +45,6 @@ def preprocess_email(email_content):
     return email_content
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
